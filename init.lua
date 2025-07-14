a0f46e6014ab2b8c36c59f5229ca414db55d9e6f
0xB4DB47 <lpenapenn@gmail.com>
2025-07-14 14:30:05 -0400

    sorbet was a bust... switched to rosepine

--- init.lua
+++ init.lua
@@ -885,11 +885,12 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'rktjmp/sorbet.nvim',
+    'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    enabled = false,
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('sorbet').setup {
+      require('tokyonight').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
@@ -898,10 +899,17 @@ require('lazy').setup({
       -- L ad the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'sorbet'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
+  -- {
+  --   'custom.sorbet'
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'sorbet'
+  --   end,
+  -- },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -988,7 +996,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
