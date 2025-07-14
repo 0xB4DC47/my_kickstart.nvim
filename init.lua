c75f253bfc6279d7ef380aceec6f86e61a6cec52
0xB4DB47 <lpenapenn@gmail.com>
2025-07-14 13:39:13 -0400

    Changed colorscheme to sorbet

--- init.lua
+++ init.lua
@@ -412,7 +412,11 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          colorscheme = {
+            enable_preview = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -881,31 +885,22 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'rktjmp/sorbet.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('sorbet').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
       }
 
-      -- Load the colorscheme here.
+      -- L ad the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
+      vim.cmd.colorscheme 'sorbet'
     end,
   },
-
-  -- {
-  --   'Rigellute/shades-of-purple.vim',
-  --   lazy = false, -- load at startup
-  --   priority = 1000, -- make sure it loads first
-  --   config = function()
-  --     vim.cmd 'colorscheme shades_of_purple'
-  --   end,
-  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
