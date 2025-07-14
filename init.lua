0993f3478e87ea77a23278fa6c32aa781652594c
0xbadbad <lpenapenn@gmail.com>
2025-07-13 21:56:59 -0400

    changed colorscheme to shades of purple clone - testing to see if it works the way i want

--- init.lua
+++ init.lua
@@ -876,27 +876,52 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+  -- {
+  --   'unrealjo/neovim-purple',
+  --   name = 'neovim-purple',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('neovim-purple').setup {
+  --       transparent = false, -- or true for transparent background
+  --       styles = {
+  --         comments = { italic = false },
+  --       },
+  --     }
+  --     vim.opt.termguicolors = true
+  --     vim.cmd 'colorscheme neovim_purple'
+  --   end,
+  -- },
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+  {
+    'Rigellute/shades-of-purple.vim',
+    lazy = false, -- load at startup
+    priority = 1000, -- make sure it loads first
+    config = function()
+      vim.cmd 'colorscheme shades_of_purple'
     end,
   },
+
+  -- {
+  --   'unrealjo/neovim-purple',
+  --   name = 'neovim-purple',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   lazy = false,
+  --   config = function()
+  --     require('neovim-purple').setup {
+  --       transparent = false,
+  --       styles = {
+  --         comments = { italic = false }, -- Disable italics in comments
+  --       },
+  --     }
+  --
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.opt.termguicolors = true
+  --     vim.cmd.colorscheme 'neovim_purple'
+  --   end,
+  -- },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -974,10 +999,10 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
